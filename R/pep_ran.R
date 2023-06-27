0c9553b9d257c5f85ff8add524c914f8c9c5ef49
leonjessen <leon.jessen@gmail.com>
2023-06-27 11:38:57 +0200

    Adding first function pep_ran

---
+++ R/pep_ran.R
@@ -0,0 +1,25 @@
+pep_ran = function(n, k){
+
+  # Define the standard 20 amino acid residue characters
+  res_chars <- c("A", "R", "N", "D", "C", "Q", "E", "G", "H", "I",
+                 "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V")
+
+  # First we generate one long vector with all the sampled residues
+  smpl_chars <- sample(res_chars, size = n*k, replace = TRUE)
+
+  # Then we collapse into one long string
+  smpl_string <- paste0(smpl_chars, collapse = '')
+
+  # Now we generate indices corresponding to extracting every 9 residues
+  to_index <- seq(from = k, to = k*n, by = k)
+  from_index <- to_index - (k-1)
+
+  # and extract using sub string
+  ran_peptides <- substring(text = smpl_string,
+                            first = from_index,
+                            last = to_index)
+
+  # Done, return
+  return( ran_peptides )
+
+}
